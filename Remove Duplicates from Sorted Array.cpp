dee0bf19ca194584d32761470989840acb0e9560
LOKESH865 <72989897+LOKESH865@users.noreply.github.com>
2021-07-03 00:18:11 +0530

    Add files via upload
    Remove duplicates from sorted array program in C++.

---
+++ Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
