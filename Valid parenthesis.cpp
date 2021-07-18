5cb4795e65d7dad149a654f7661fc7bf684119e3
LOKESH865 <72989897+LOKESH865@users.noreply.github.com>
2021-07-18 11:12:03 +0530

    Add files via upload
    leetcode valid parenthesis program

---
+++ Valid parenthesis.cpp
@@ -0,0 +1,29 @@
+  #include<iostream>
+using namespace std;
+  
+  class Solution {
+public:
+    bool isValid(string s) 
+    {
+        string str;
+        for (int i = 0; i < s.size(); ++ i) 
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                str.push_back(s[i]);
+            else 
+            {
+                if (s[i] == ')' && (!str.size() || str[str.size() - 1] != '(')) 
+                    return false; 
+                else if (s[i] == '}' && (!str.size() || str[str.size() - 1] != '{')) 
+                    return false; 
+                else if (s[i] == ']' && (!str.size() || str[str.size() - 1] != '[')) 
+                    return false;
+                str.pop_back();
+            }
+        }
+        if (!str.size()) 
+            return true;
+        return false;
+    }
+};
+
