2803cdbd4bcf733ff322e9db43f2c974e52a15ca
LOKESH865 <72989897+LOKESH865@users.noreply.github.com>
2021-07-31 11:14:28 +0530

    Add files via upload
    Remove element leetcode problem .

---
+++ Remove element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int elem: nums){
+            if (elem != val){
+                nums[i] = elem;                
+                i++;
+            }
+        }
+        return i;
+    }
+};
